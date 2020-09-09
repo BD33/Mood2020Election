b4e510cb1f8a5cb209c4e61d3a80a50e18c6fb4b
Billy DeNiro <billydeniro3@gmail.com>
2020-09-08 20:43:41 -0400

    Added Search Component

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Search from './Search'
 function App() {
   return (
     <div className="App">
+          <Search />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Watsons News API Tool
         </p>
         <a
           className="App-link"
