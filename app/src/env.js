27445abf8a77d806dbdfa4bb8c7ff02035aaa7ad
Michael Mendiola <mendiola.67@osu.edu>
2020-09-09 21:20:19 -0400

    api auth and env vars setup

---
+++ app/src/env.js
@@ -0,0 +1,10 @@
+const DiscoveryV2 = require('ibm-watson/discovery/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const discovery = new DiscoveryV2({
+  version: '2020-08-27',
+  authenticator: new IamAuthenticator({
+    apikey: process.env.REACT_APP_DISCOVERY_API_KEY,
+  }),
+  serviceUrl: process.env.REACT_APP_DISCOVERY_URL,
+});
